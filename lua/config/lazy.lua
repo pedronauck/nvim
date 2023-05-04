9163e0c339224bfc3d81ca46ec455ff2b26a5d62
Pedro Nauck <pedronauck@gmail.com>
2023-05-04 11:41:55 -0300

    chore: config changes

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,9 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.coding.copilot" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.linting.eslint" },
+		{ import = "lazyvim.plugins.extras.dap.core" },
+		{ import = "lazyvim.plugins.extras.dap.nlua" },
+		{ import = "lazyvim.plugins.extras.util.project" },
 		-- { import = "lazyvim.plugins.extras.ui.mini-starter" },
 		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
 		-- import/ogerride with your plugins
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,10 +1,10 @@
 return {
-	{
-		"folke/persistence.nvim",
-		enabled = false,
-	},
-	{
-		"rcarriga/nvim-notify",
-		enabled = false,
-	},
+	-- {
+	-- 	"folke/persistence.nvim",
+	-- 	enabled = false,
+	-- },
+	-- {
+	-- 	"rcarriga/nvim-notify",
+	-- 	enabled = false,
+	-- },
 }
